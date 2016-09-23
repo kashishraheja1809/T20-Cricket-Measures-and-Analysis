5c79b177b3a4647818e79745beac4e88077b3f8d
Kashish Raheja <kashishraheja1809@gmail.com>
2016-09-23 15:00:21 +0530

    Add files via upload

---
+++ Codes/Average.R
@@ -0,0 +1,98 @@
+#df<-read.table("/home/kashish/Downloads/t20_csv/298795.csv", header = FALSE, sep = ",", col.names = paste0("V",seq_len(11)), fill = TRUE)
+
+
+setwd("/home/kashish/Downloads/t20_csv")
+file_list<-list.files()
+average <- array(dim=c(1,10))
+no_of_innings <- array(dim=c(1,10))
+no_of_innings <- c(0,0,0,0,0,0,0,0,0,0)
+
+for (file in file_list){
+  
+  #if the merged dataset doesn't exist, create it
+  if (!exists("dataset")){
+    dataset <- read.table(file, header=FALSE, sep=",",quote='',col.names = paste0("V",seq_len(11)),fill=TRUE)
+  }
+  else
+  {
+    # if the merged dataset does exist, append to it
+    #if (exists("dataset")){
+    temp_dataset <-read.table(file, header=FALSE,  sep=",",quote='',col.names = paste0("V",seq_len(11)),fill=TRUE)
+    dataset<-rbind(dataset, temp_dataset)
+    rm(temp_dataset)
+  }
+  
+}
+
+ndataset <- subset(dataset, (dataset[,2]==1 | dataset[,2]==2))
+temp1 =0
+sum1 = 0
+count1 = 0
+
+
+for(i in 1:nrow(ndataset))
+{
+  if(ndataset[i,1]=="version")
+  {
+    ndataset[i,8] = 0
+    ndataset[i,9] = 0
+  }
+}
+match = 0
+average = c(0,0,0,0,0,0,0,0,0,0)
+for(i in 1:nrow(ndataset))
+{
+  if(ndataset[i,1]=="version")
+  {
+    no_of_innings[1] = no_of_innings[1] + 1
+    temp1 = 0
+    match= match +1
+    count1 = 0
+  }
+  
+  if(ndataset[i,2]==1)
+  {
+    if(temp1 == 0)
+    {
+      average[count1+1]=average[count1+1]+ndataset[i,8]
+      average[count1+1]=average[count1+1]+ndataset[i,9]
+    }
+    
+    if(ndataset[i,10] == 'caught' | ndataset[i,10] == 'bowled' | ndataset[i,10] == 'lbw' | ndataset[i,10] == 'run out')
+    {
+      no_of_innings[(count1+2)%%10] = no_of_innings[(count1+2)%%10] + 1
+      count1= count1+1
+    }
+  }
+  
+}
+
+
+for(i in 1:nrow(ndataset))
+{
+  if(ndataset[i,1]=="version")
+  {
+    no_of_innings[1] = no_of_innings[1] + 1
+    temp1 = 0
+    match= match +1
+    count1 = 0
+  }
+
+  if(ndataset[i,2]==2)
+  {
+    if(temp1 == 0)
+    {
+      average[count1+1]=average[count1+1]+ndataset[i,8]
+      average[count1+1]=average[count1+1]+ndataset[i,9]
+    }
+
+    if(ndataset[i,10] == 'caught' | ndataset[i,10] == 'bowled' | ndataset[i,10] == 'lbw' | ndataset[i,10] == 'run out')
+    {
+      no_of_innings[(count1+2)%%10] = no_of_innings[(count1+2)%%10] + 1
+      count1= count1+1
+    }
+  }
+
+}
+
+Avg = average/no_of_innings
---
+++ Codes/T20_Analysis14.R
@@ -0,0 +1,73 @@
+# Scorecard Link: http://www.espncricinfo.com/indian-premier-league-2016/engine/match/981005.html
+
+mydata = read.csv("/home/kashish/Downloads/T20Matches/981005_req14.csv",row.names = NULL, header = F,skip = 20,colClasses = c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+
+sum1 = 0
+for( i in 1:nrow(mydata))
+{
+  if(mydata[i,1]==1)
+  {
+    sum1 = sum1 + mydata[i,7]
+    sum1 = sum1 + mydata[i,8]
+  }
+}
+target = sum1+1
+wickets = array()
+j = 1
+runs_required = array()
+inc = 1
+rate = c(1.302922, 1.311687, 1.289697, 1.372977, 1.451710, 1.515652, 1.563730, 1.712957, 1.479592, 1.411862)
+wk_fell = 0
+pres <- array()
+sum2 = 0
+k=1
+
+wicket_part <- array(dim = c(1,10))
+
+wicket_part[1:10] = c(25.500698, 26.689417, 25.145484, 21.861925, 19.394195, 14.127869, 10.743169,  9.443223,  6.109551, 4.23423)
+x = sum(wicket_part[wk_fell:10])
+no_of_balls = 120
+for (i in 1:nrow(mydata))
+{
+  if(mydata[i,1]==2)
+  {
+    sum2 = sum2 + mydata[i,7]
+    sum2 = sum2 + mydata[i,8]
+    runs_required[inc] = target - sum2
+    inc = inc +1
+    avg_rate = rate[wk_fell+1]
+    
+    pres[k]= (((((target-sum2)/no_of_balls)/avg_rate) + ((target - sum2)/(sum(wicket_part[(wk_fell+1):10]))))/2)*100
+    
+    k = k+1 
+    if(mydata[i,8]==0)
+    {
+      no_of_balls= no_of_balls - 1
+    }
+    
+    if(mydata[i,9]!='')
+    {
+      wk_fell = wk_fell + 1
+      wickets[j] = 120 - no_of_balls
+      j = j+1
+    }
+    
+  }
+  
+}
+#pres <- head(pres,-1)
+#runs_required <- head(runs_required, -1)
+balls = 1:length(pres)
+par(mar = c(5,5,2,5))
+plot(balls, pres, xlab = "", ylab= "",axes = FALSE,col = ifelse(balls == wickets[1] | balls == wickets[2] | balls == wickets[3] | balls == wickets[4] | balls == wickets[5] | balls == wickets[6] , "red", "black"), pch=ifelse(balls == wickets[1] | balls == wickets[2] | balls == wickets[3] | balls == wickets[4] | balls == wickets[5] | balls == wickets[6] , 19, 1), cex=ifelse(balls == wickets[1] | balls == wickets[2] | balls == wickets[3] | balls == wickets[4] | balls == wickets[5] | balls == wickets[6] , 1.5, 1))
+lines(balls,pres)
+axis(2, ylim = c(0, max(pres)), las=1)
+mtext("Pressure", side = 2, line = 2.5)
+box()
+par(new = T)
+plot(balls, runs_required,xlab = "", ylab ="", axes = FALSE, col="red")
+lines(balls, runs_required,col = "red")
+axis(side = 4,las = 1, col.axis = "red")
+mtext(side = 4, line = 2.5, 'Runs Required', col = "red")
+axis(1, xlim = 1:length(pres))
+mtext("Balls", side = 1, line = 2.5)
